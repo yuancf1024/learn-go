93ab13e3527e6ad3454e6046d14e2fb356305131
yuancf1024 <3455911441@qq.com>
2022-01-14 16:09:22 +0800

    增加“并行基准测试”

--- Go-simple/Go-Test/benchmarkTest/main_test.go
+++ Go-simple/Go-Test/benchmarkTest/main_test.go
@@ -3,12 +3,26 @@ package main
 import (
 	"testing"
 	"fmt"
+	// "template"
 )
 
 func BenchmarkHello(b *testing.B) {
-	... // 耗时操作
+	// ... // 耗时操作
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		fmt.Sprintf("hello")
 	}
+}
+
+// 下面的并行基准测试代码无法正常运行
+func BenchmarkParallel(b *testing.B) {
+	templ := template.Must(template.New("test").Parse("Hello, {{.}}!"))
+	b.RunParallel(func(pb *testing.PB){
+		var buf bytes.Buffer
+		for pb.Next() {
+			// 所有 goroutine 一起，循环一共执行 b.N 次
+			buf.Reset()
+			templ.Execute(&buf, "World")
+		}
+	})
 }
